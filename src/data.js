2e745f6f5de4a501c6c4c788062fd9b737424716
raianushka <96331163+raianushka@users.noreply.github.com>
2024-09-03 18:27:38 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const reviews = [
+    {
+      id:1,               
+      name:"Pranay Gupta",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+    {
+      id:2,               
+      name:"Abir Pal",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+    {
+      id:3,               
+      name:"Soumya Banerjee",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+    {
+      id:4,               
+      name:"Pran Gupta",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+    {
+      id:5,               
+      name:"Prana Gupta",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+    {
+      id:6,               
+      name:"Pranay Gupt",  
+      job: "Software Engineer",
+      image:"https://th.bing.com/th/id/OIP.-pPriHSWqjJxJ0mcAxAUBgHaFg?pid=ImgDet&rs=1",
+      text: "Jaipur , formerly Jeypore, is the capital and largest city of the Indian state of Rajasthan. As of 2011 , the city had a population of 3.1 million, making it the tenth most populous city in the country. Jaipur is also known as the Pink City, due to the dominant colour scheme of its buildings. It is also known as the Paris of India, and C. V. Raman called it the Island of Glory. It is located 268 km (167 miles) from the national capital New Delhi. "
+    },
+
+
+];
+
+export default reviews;
